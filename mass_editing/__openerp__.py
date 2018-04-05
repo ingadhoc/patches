db122ea65b074c2b498d1b115a1234477962aee9
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-04-05 19:18:27 -0300

    [REF] increase mass editting manifest version

--- mass_editing/__openerp__.py
+++ mass_editing/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Mass Editing',
-    'version': '9.0.1.0.0',
+    'version': '9.0.1.1.0',
     'author': 'Serpent Consulting Services Pvt. Ltd., '
               'Odoo Community Association (OCA)',
     'contributors': [
