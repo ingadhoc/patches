e88300dab01f3e0d2f49922743edfa9c1c311049
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2018-07-02 15:31:13 -0300

    [FIX] change isbn to barcode in tree and search view (#14)

--- logos_invoice_analysis/__openerp__.py
+++ logos_invoice_analysis/__openerp__.py
@@ -2,7 +2,7 @@
 {
     'name': 'Logos Invoice Analysis',
     'category': 'Account',
-    'version': '9.0.1.1.0',
+    'version': '9.0.1.2.0',
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
