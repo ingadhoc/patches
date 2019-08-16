1b779c9b8ed8f23aa0d936a5562f4f938fa20085
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-08-16 17:46:09 -0300

    [REF] base_name_search_improved: update manifest version

--- base_name_search_improved/__manifest__.py
+++ base_name_search_improved/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Improved Name Search',
     'summary': 'Friendlier search when typing in relation fields',
-    'version': '11.0.1.1.1',
+    'version': '12.0.1.0.0',
     'category': 'Uncategorized',
     'website': 'https://odoo-community.org/',
     'author': 'Daniel Reis, Odoo Community Association (OCA), ADHOC SA',
