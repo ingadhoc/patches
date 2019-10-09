4174fd9c2f997abe40306ec58141e1234642be3c
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2019-10-08 21:24:02 -0300

    [FIX] hr_skills: some fixes related the backport (#39)
    - Date() it's not used in v11 becase if an string
    - Create with a list of dict are not avalible in v11, create only one by one
    - The job_title not exist in v11, use the description of a job

--- hr_skills/models/hr_employee.py
+++ hr_skills/models/hr_employee.py
@@ -13,15 +13,15 @@ class Employee(models.Model):
     @api.model
     def create(self, values):
         res = super().create(values)
-        resume_lines_values = []
         for employee in res:
-            line_type = self.env.ref('hr_skills.resume_type_experience', raise_if_not_found=False)
-            resume_lines_values.append({
+            line_type = self.env.ref(
+                'hr_skills.resume_type_experience', raise_if_not_found=False)
+            resume_lines_values = {
                 'employee_id': employee.id,
                 'name': employee.company_id.name or '',
-                'date_start': employee.create_date.date(),
-                'description': employee.job_title or '',
+                'date_start': employee.create_date,
+                'description': employee.job_id.description or '',
                 'line_type_id': line_type and line_type.id,
-            })
-        self.env['hr.resume.line'].create(resume_lines_values)
+            }
+            self.env['hr.resume.line'].create(resume_lines_values)
         return res
